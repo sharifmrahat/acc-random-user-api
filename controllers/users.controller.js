862c55eaca1698cca1ab45b3eb68f33bab235198
ShareefRahat <imr4h47@gmail.com>
2022-09-04 17:16:38 +0600

    limit query is added for getAllUsers

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,11 @@
 const usersData = require("../utils/usersDB");
 
 module.exports.getAllUsers = async (req, res) => {
+  const limit = req.query.limit;
+
   const users = await usersData.getUsersData();
+  if (limit) {
+    return res.send(users.slice(0, limit));
+  }
   res.send(users);
 };
