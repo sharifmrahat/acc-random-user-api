be53b0fd8339ea4fa17ea9edd8868dc321d331ff
ShareefRahat <imr4h47@gmail.com>
2022-09-04 12:26:23 +0600

    indexjs file is setup

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Random User Project");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.all("*", (req, res) => {
+  res.send({ error: "No route found" });
+});
+app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
+
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
